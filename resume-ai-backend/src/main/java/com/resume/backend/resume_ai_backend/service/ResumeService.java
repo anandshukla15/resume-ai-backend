fcd2b96c74535120f8be200414d996981a9c5b2c
anandshukla15 <anand.230102013@iiitbh.ac.in>
2025-08-12 23:15:30 +0530

    setup first commit

---
+++ resume-ai-backend/src/main/java/com/resume/backend/resume_ai_backend/service/ResumeService.java
@@ -0,0 +1,5 @@
+package com.resume.backend.resume_ai_backend.service;
+
+public interface ResumeService {
+    String generateResumeResponse(String userResumeDescription);
+}
---
+++ resume-ai-backend/src/main/java/com/resume/backend/resume_ai_backend/service/ResumeServiceImpl.java
@@ -0,0 +1,13 @@
+package com.resume.backend.resume_ai_backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResumeServiceImpl implements ResumeService{
+
+    @Override
+    public String generateResumeResponse(String userResumeDescription){
+        return " ";
+    }
+
+}
